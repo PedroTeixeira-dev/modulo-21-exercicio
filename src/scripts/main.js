f63afd2bb736cba8f965fcf440f25617e82074da
Pedro Teixeira <pedro15at@gmail.com>
2023-10-04 15:51:29 -0300

    "inicio do exercicio"

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const texto = document.getElementById('texto')
+    setTimeout(function(){
+        texto.style.opacity = 1
+    }, 3000)
+})
